e54b1e47291823c24f1c31da2d3c836539200963
Ung Wei Liang <liang2010@hotmail.my>
2021-09-28 18:55:27 +0800

    init with electron, vue, photon

---
+++ real-world-2.0/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div class="window">
+    <div class="window-content">
+      <div class="pane-group">
+        <div class="pane-sm sidebar">
+
+          <nav class="nav-group">
+            <router-link class="nav-group-item" to="/">Home</router-link>
+            <router-link class="nav-group-item" to="/about">About</router-link>
+          </nav>
+          
+        </div>
+        <div class="pane">
+          <router-view/>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ real-world-2.0/src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button type="button" @click="openFolder">Open Folder</button>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.ipc.on("READ_FOLDER_PATH", (payload) => {
+      console.log(payload);
+    });
+  },
+  methods: {
+      openFolder: function () {
+        window.ipc.send("READ_FOLDER_PATH", {"test": "test"} );
+      }
+  }
+}
+</script>
