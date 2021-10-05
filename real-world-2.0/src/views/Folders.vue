3f385b2e1ab25d26e7c2ee5139671ad27b1b445e
ling <lingxiao3218@gmail.com>
2021-10-05 15:55:12 +0800

    feat: folders.vue add button for action row

--- real-world-2.0/src/views/Folders.vue
+++ real-world-2.0/src/views/Folders.vue
@@ -9,7 +9,15 @@
       </div>
     </div>
     <div class="apptable">
-      <b-table striped hover :items="items" :fields="fields" @row-clicked="clickHandler"></b-table>
+      <b-table striped hover :items="items" :fields="fields" @row-clicked="clickHandler">
+
+          <template #cell(action)>
+            <b-button size="sm" @click="click_detail_btn">
+                Delete
+            </b-button>
+          </template>
+
+      </b-table>
     </div>
   </div>
 </template>
@@ -18,16 +26,19 @@
 export default {
   data() {
     return {
-      fields: ['no', 'folder_name', 'details', 'created_date', 'action'],
+      fields: ['folder_name', 'path', 'created_date', 'action'],
       items: [
-        { no: 1, folder_name: "folder1", details: "details_text", created_date: "01/01/2012", action: "" },
-        { no: 2, folder_name: "folder2", details: "details_text", created_date: "01/01/2012", action: "" },
+        { folder_name: "folder1", path: "c:/sasaa/sasas/asasas/",  created_date: "01/01/2012", action: "" },
+        { folder_name: "folder2", path: "c:/sasaa/sasas/asasas/",  created_date: "01/01/2012", action: "" },
       ],
     };
   },
   methods: {
     click_new_folder() {
       alert("new folder is clicked! this should open a dialog for user to select folder")
+    },
+    click_detail_btn: () => {
+      alert("YO 🥒🥒🥒🥒🥒🥒");
     }
   }
 };
