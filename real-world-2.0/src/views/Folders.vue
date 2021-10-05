446620a625ce6e857c43f8c663c73344c93ddd38
ling <lingxiao3218@gmail.com>
2021-10-05 16:35:48 +0800

    feat: folders.vue loading overlay

--- real-world-2.0/src/views/Folders.vue
+++ real-world-2.0/src/views/Folders.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="folder">
-    <div class="header">
-      <div class="d-flex bg-header pt-5 pb-4">
-        <div class="flex-column ml-5 d-flex justify-content-start">
-          <h1>Folders</h1>
-          <button type="button" class="btn btn-primary btn-lg mt-2" @click="click_new_folder">New Folder</button>
+    <div>
+      <b-overlay :show="showLoading">
+        <div class="header">
+          <div class="d-flex bg-header pt-5 pb-4">
+            <div class="flex-column ml-5 d-flex justify-content-start">
+              <h1>Folders</h1>
+              <button type="button" class="btn btn-primary btn-lg mt-2" @click="click_new_folder">New Folder</button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    <div class="apptable">
-      <b-table striped hover :items="items" :fields="fields" @row-clicked="clickHandler">
-
-          <template #cell(action)>
-            <b-button size="sm" @click="click_detail_btn">
-                Delete
-            </b-button>
-          </template>
-
-      </b-table>
+        <div class="apptable">
+          <b-table striped hover :items="items" :fields="fields" @row-clicked="clickHandler">
+            <template #cell(action)>
+              <b-button size="sm" @click="click_detail_btn"> Delete </b-button>
+            </template>
+          </b-table>
+        </div>
+      </b-overlay>
     </div>
   </div>
 </template>
@@ -26,21 +26,23 @@
 export default {
   data() {
     return {
-      fields: ['folder_name', 'path', 'created_date', 'action'],
+      fields: ["folder_name", "path", "created_date", "action"],
       items: [
-        { folder_name: "folder1", path: "c:/sasaa/sasas/asasas/",  created_date: "01/01/2012", action: "" },
-        { folder_name: "folder2", path: "c:/sasaa/sasas/asasas/",  created_date: "01/01/2012", action: "" },
+        { folder_name: "folder1", path: "c:/sasaa/sasas/asasas/", created_date: "01/01/2012", action: "" },
+        { folder_name: "folder2", path: "c:/sasaa/sasas/asasas/", created_date: "01/01/2012", action: "" },
       ],
+      showLoading: false,
     };
   },
   methods: {
     click_new_folder() {
-      alert("new folder is clicked! this should open a dialog for user to select folder")
+      alert("new folder is clicked! this should open a dialog for user to select folder");
+      this.showLoading = true
     },
     click_detail_btn: () => {
       alert("YO 🥒🥒🥒🥒🥒🥒");
-    }
-  }
+    },
+  },
 };
 </script>
 
