842aefc73ba862a3829b37520b181c92a8058f57
ling <lingxiao3218@gmail.com>
2021-10-08 17:48:20 +0800

    fix: update files.vue

--- real-world-2.0/src/views/Files.vue
+++ real-world-2.0/src/views/Files.vue
@@ -1,38 +1,43 @@
 <template>
-    <div class="header">
-      <div class="d-flex bg-header pt-5 pb-4">
-        <div class="flex-column ml-5 d-flex justify-content-start">
-          <h1>folder1</h1>
-          <button type="button" class="btn btn-primary btn-lg mt-2" @click="click_new_folder">New Images</button>
-        </div>
+  <div class="header">
+    <div class="d-flex bg-header pt-5 pb-4">
+      <div class="flex-column ml-5 d-flex justify-content-start">
+        <h1>folder1</h1>
+        <h5>10 images pending, 2 images pass, 0 images failed</h5>
+
+        <!-- NOW TABLE DISPLAYING is for debug only! not a feature! -->
+        <h5>NOW TABLE DISPLAYING: {{ tableIsDisplaying }}</h5>
+        
+        <button type="button" class="btn btn-primary btn-lg mt-2" @click="click_new_folder">New Images</button>
       </div>
+    </div>
 
-<nav class="navbar navbar-dark bg-dark">
-  <!-- Navbar content -->
-   <button class="btn btn-success me-2 btn-lg" type="button" data-target="#Table" >Pending</button>
-   
-          <button class="btn btn-success me-2 btn-lg" type="button">Failed</button>
-          
-          <button class="btn btn-success me-2 btn-lg" type="button">Passed</button>
-          
-          <button class="btn btn-success me-2 btn-lg" type="button">All</button>
-  
- 
+    <nav class="bg-dark d-flex flex-row">
+      <!-- Button Tab content -->
+      <!-- <b-button-group> -->
+        <button class="btn btn-tabs btn-lg" type="button" @click="tabClicked(tabletab.pending)">Pending</button>
 
-</nav>
-   
-       <div id="Table">
-      <b-table striped hover :items="items" :fields="fields" @row-clicked="clickHandler">
+        <button class="btn btn-tabs btn-lg" type="button" @click="tabClicked(tabletab.failed)">Failed</button>
+
+        <button class="btn btn-tabs btn-lg" type="button" @click="tabClicked(tabletab.passed)">Passed</button>
+
+        <button class="btn btn-tabs btn-lg" type="button" @click="tabClicked(tabletab.all)">All</button>
+        
+      <!-- </b-button-group> -->
+    </nav>
+
+    <div id="Table">
+      <b-table striped hover :items="items" :fields="fields" @row-clicked="rowClickHandler">
         <template #cell(action)>
-          <b-button size="sm" @click="click_detail_btn"> Delete </b-button>
+          <b-button size="sm" @click="click_action_btn"> Delete </b-button>
         </template>
       </b-table>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-       export default {
+export default {
   data() {
     return {
       fields: ["Number", "IMAGE_NAME", "CREATED_DATE", "ACTION"],
@@ -41,30 +46,35 @@
         { Number: "2", IMAGE_NAME: "view.jpg", CREATED_DATE: "01/01/2012", ACTION: "bi bi-trash" },
       ],
       showLoading: false,
+      tableIsDisplaying: null,
+      tabletab: {
+        pending: "pending",
+        failed: "failed",
+        passed: "passed",
+        all: "all",
+      },
     };
   },
   methods: {
     click_new_folder() {
-      alert("new folder is clicked! this should open a dialog for user to select folder");
-      this.overlayBlocking()
+      alert("new image is clicked!");
     },
-    click_detail_btn: () => {
-      alert("YO 🥒🥒🥒🥒🥒🥒");
+    click_action_btn() {
+      alert("delete button clicked");
     },
-    overlayBlocking() {
-      this.showLoading = !this.showLoading;
-      this.$store.commit("overlayBlockingAppSwitch", this.showLoading);
+    rowClickHandler() {
+      alert("row clicked");
+    },
+    tabClicked: function (tabname) {
+      console.log(tabname);
+      this.tableIsDisplaying = tabname;
     },
   },
-};    
+};
 </script>
 
 
 <style scoped>
-.btn:hover {
-  cursor: pointer;
-}
-
 .bg-header {
   background-color: rgb(204, 253, 237);
 }
@@ -72,4 +82,36 @@
 .flex-column {
   align-items: flex-start;
 }
+
+.btn-tabs {
+  color: #fff;
+  background-color: #ff9494;
+  /* background-color: #fc8c8c; */
+  border-radius: 0rem;
+  width: 10rem;
+}
+
+.btn-lg {
+  padding: 1rem 2rem;
+  font-size: 1.25rem;
+  line-height: 1.5;
+}
+  
+  .btn:hover {
+    color: #fff;
+    background-color: #fc8181;
+    transform: translateY(-2px);
+    transition: 0.3s;
+    transition-timing-function: ease-in-out;
+  }
+
+.btn:focus {
+  box-shadow: 0 0 0 0 #ffc7c7;
+  background-color: #e76e6e;
+}
+
+/* .btn:focus {
+  box-shadow: 0 0 0 0 ;
+  background-color: #dd6060;
+} */
 </style>
