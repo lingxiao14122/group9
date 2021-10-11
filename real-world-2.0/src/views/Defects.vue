89d2fd3f188dde97ce04fa5b8dbe1817cd44efa6
ling <lingxiao3218@gmail.com>
2021-10-11 11:08:15 +0800

    feat: defect category modal form

--- real-world-2.0/src/views/Defects.vue
+++ real-world-2.0/src/views/Defects.vue
@@ -5,7 +5,7 @@
         <div class="flex-column ml-5 d-flex justify-content-start">
           <h1>Defect Category</h1>
           <div class="d-flex">
-            <button type="button" class="justify-content-start btn btn-primary btn-lg mt-2" @click="header_click">
+            <button type="button" class="justify-content-start btn btn-primary btn-lg mt-2" v-b-modal.modal-defect-form>
               New Category
             </button>
           </div>
@@ -19,6 +19,20 @@
         </template>
       </b-table>
     </div>
+    <div class="appmodal">
+      <b-modal v-model="showModal" id="modal-defect-form" title="New Defect Category">
+        <b-form id="formm" @submit="onSubmit" @reset="onReset">
+          <b-form-group id="input-group-1" label="Defect Name:" label-for="input-1">
+            <b-form-input id="input-1" v-model="form.defectName" placeholder="Enter defect name" required>
+            </b-form-input>
+          </b-form-group>
+        </b-form>
+        <template #modal-footer>
+          <b-button type="reset" form="formm">Reset</b-button>
+          <b-button type="submit" form="formm" variant="primary">Submit</b-button>
+        </template>
+      </b-modal>
+    </div>
   </div>
 </template>
 
@@ -31,19 +45,29 @@ export default {
         { key: "action", label: "Action" },
       ],
       items: [{ name: "defect1" }, { name: "defect2" }],
+      form: {
+        defectName: "",
+      },
+      showModal: false,
     };
   },
   methods: {
-    clickHandler() {},
-    header_click() {},
+    onSubmit(event) {
+      event.preventDefault();
+      // alert(JSON.stringify(this.form));
+      this.showModal = false;
+      this.items.push({name: this.form.defectName})
+      this.onReset();
+    },
+    onReset() {
+      this.form.defectName = '';
+    },
   },
 };
 </script>
 
 <style scoped>
 .bg-header {
-  background-color: rgb(182, 201, 253) ;
-  /* background-color: #96fcc0 ; */
-  /* background-color: #d396fc ; */
+  background-color: rgb(182, 201, 253);
 }
 </style>
