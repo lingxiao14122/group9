5930537fbda67f16333d8997ce5f6767d7e2ec3e
ling <lingxiao3218@gmail.com>
2021-10-29 16:52:37 +0800

    fix: remove unused routes

--- real-world-2.0/src/App.vue
+++ real-world-2.0/src/App.vue
@@ -2,12 +2,8 @@
   <b-overlay :show="overlayBlocking">
     <div id="app" class="d-flex">
       <div id="nav" class="sidebar d-flex flex-column">
-        <router-link to="/home">Home</router-link>
-        <router-link to="/about">About</router-link>
         <router-link to="/folders">Folders</router-link>
-        <router-link to="/files">Files</router-link>
         <router-link to="/defects">Defects</router-link>
-        <router-link to="/segregation">Segregation</router-link>
       </div>
       <div id="appbody" class="flex-grow-1 bg-appbody">
         <router-view />
--- real-world-2.0/src/router/index.js
+++ real-world-2.0/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Folders from '../views/Folders.vue'
 import Files from '../views/Files.vue'
 import Defects from '../views/Defects.vue'
@@ -9,19 +8,6 @@ import Segregation from '../views/Segregation.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
   {
     path: '/folders',
     name: 'Folders',
